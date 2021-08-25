40a35841f4c466cb6562b302627028443cbe853d
Łukasz Kapiński <kapinski.l@gmail.com>
2021-08-25 22:14:54 +0200

    Added weather service

---
+++ Services/IWeatherForecastService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantAPI.Services
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get();
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestaurantAPI.Services;
 
 namespace RestaurantAPI
 {
@@ -25,7 +26,7 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<IWeatherForecastService, WeatherForecastService>();
             services.AddControllers();
         }
 
