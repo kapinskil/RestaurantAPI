8584ce0e92ee8623e90276c98b1ff4e8563c1110
Łukasz Kapiński <kapinski.l@gmail.com>
2021-10-20 23:06:46 +0200

    Added Role Entity and seed of roles

---
+++ Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public String Nationality { get; set; }
+        public string MyProperty { get; set; }
+        public string PasswordHas { get; set; }
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
--- RestaurantSeeder.cs
+++ RestaurantSeeder.cs
@@ -17,15 +17,34 @@ namespace RestaurantAPI
 
         public void Seed()
         {
-            if(_dbContext.Database.CanConnect())
+            if (_dbContext.Database.CanConnect())
             {
-                if(!_dbContext.Restaurants.Any())
+                if (!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+
+                if (!_dbContext.Restaurants.Any())
                 {
                     var restaurants = GetRestaurants();
                     _dbContext.Restaurants.AddRange(restaurants);
                     _dbContext.SaveChanges();
                 }
             }
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role() {Name = "User"},
+                new Role() { Name = "Manager" },
+                new Role() { Name = "Administarator" }
+            };
+
+            return roles;
         }
 
         private IEnumerable<Restaurant> GetRestaurants()
