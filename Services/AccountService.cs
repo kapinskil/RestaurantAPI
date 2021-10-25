80f2c139efd11c1872a34baaba02288a61707f89
Łukasz Kapiński <kapinski.l@gmail.com>
2021-10-25 21:11:54 +0200

    Added register user service

---
+++ Services/AccountService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDbContext _context;
+
+        public AccountService(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public void RegisterUser(RegisterUserDto dto)
+        {
+            var user = new User
+            {
+                Email = dto.Email,
+                DateOfBirth = dto.DateOfBirth,
+                Nationality = dto.Nationality,
+                RoleId = 1
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+    }
+}
